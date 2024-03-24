507ae96af3cfdd117af5214d03312a69feb2ad77
Rishabh <n11494468@qut.edu.au>
2024-03-24 16:46:37 +1000

    Rishabh changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import common.Location;
 import common.Map;
-import obstacles.Guard;
-import obstacles.Obstacle;
-import obstacles.ObstacleType;
+import obstacles.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,17 +37,23 @@ class Main {
      */
     public static ArrayList<Obstacle> parseObstacles(HashMap<String, ArrayList<String>> parsedArgs) {
         ArrayList<Obstacle> obstacles = new ArrayList<>();
-        ObstacleType type = ObstacleType.GUARD;
-        String key = "-" + type.getArgumentName();
-        ArrayList<String> args = parsedArgs.get(key);
-        if (args == null) {
-            return obstacles;
-        }
-        for (String arg : args) {
-            // Remove the parentheses from the argument
-            String cleanedArg = stripParentheses(arg);
-            Obstacle obstacle = Guard.parse(cleanedArg);
-            obstacles.add(obstacle);
+        for (ObstacleType type : ObstacleType.values()) {
+            String key = "-" + type.getArgumentName();
+            ArrayList<String> args = parsedArgs.get(key);
+            if (args == null) {
+                continue;
+            }
+            for (String arg : args) {
+                // Remove the parentheses from the argument
+                String cleanedArg = stripParentheses(arg);
+                Obstacle obstacle = switch (type) {
+                    case GUARD -> Guard.parse(cleanedArg);
+                    case FENCE -> Fence.parse(cleanedArg);
+                    case SENSOR -> Sensor.parse(cleanedArg);
+                    case CAMERA -> Camera.parse(cleanedArg);
+                };
+                obstacles.add(obstacle);
+            }
         }
         return obstacles;
     }
